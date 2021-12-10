15949e4c19ce4f90b7c52e4a67e77a98792031ff
Stephen Rooks <stephenp.rooks@hotmail.com>
2021-12-10 12:47:37 -0500

    Updating moon - current date will soon set moon phase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-fake
+window.onload = function(){
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  var day = today.getDate();
+
+  phase = getMoonPhase(year, month, day) + 1;
+  console.log(phase);
+}
